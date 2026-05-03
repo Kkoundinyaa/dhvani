14c3864d194ce170ea050cb9d302ade891f30682
Krishna Badikela <orugantikoundinya7@gmail.com>
2026-05-03 19:26:18 -0400

    Bump to v0.2.2 with updated PyPI landing page

--- dhvani/__init__.py
+++ dhvani/__init__.py
@@ -1,5 +1,5 @@
 """dhwani - Phonetic normalization toolkit for Hinglish text."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from dhvani.core import normalize, to_ipa, to_devanagari, are_same, identify_languages
